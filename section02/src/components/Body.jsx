bd0fa8053cab6926dfbd544b1cd40b9366ae5df4
hyeran <gpfksdlrn@naver.com>
2023-11-10 17:18:12 +0900

    section03 React의 State/Props/Ref

--- section02/src/components/Body.jsx
+++ section02/src/components/Body.jsx
@@ -1,7 +1,62 @@
+import "./Body.css";
+import { useRef, useState } from "react";
+
 export default function Body() {
+    const [state, setSate] = useState({
+        name : "",
+        gender : "",
+        bio : ""
+    });
+
+    const nameRef = useRef()
+
+    const onChage = (e) => {
+        setSate({
+            ...state,
+            [e.target.name] : e.target.value,
+        })
+    }
+
+    const onSubmit = () => {
+        if(state.name === ""){
+            nameRef.current.focus();
+            return;
+        }
+        alert(`${state.name}님 회원가입을 축하합니다.`);
+    }
+
     return (
-      <div>
-        <h1>BODY</h1>
+      <div className="body">
+        <div>
+            <input 
+              ref={nameRef}
+              name={"name"}
+              value={state.name} 
+              onChange={onChage}
+            />
+        </div>
+        <div>
+            <select 
+              name={"gender"}
+              value={state.gender} 
+              onChange={onChage}
+            >
+                <option value="">밝히지 않음</option>
+                <option value="female">여성</option>
+                <option value="male">남성</option>
+            </select>
+        </div>
+        <div>
+            <textarea 
+              name={"bio"}
+              value={state.bio} 
+              onChange={onChage}
+            />
+        </div>
+        <div>
+            <button onClick={onSubmit}>회원가입</button>
+        </div>
       </div>
     );
+
 };
---
+++ section02/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import './Button.css';
+
+export default function Button({color, text, children}){
+    const onClick = (e) => {
+        console.log(e);
+    };
+
+    return (
+        <button 
+            onClick={onClick}
+            style={{
+                backgroundColor: color
+            }}
+            className='button'
+        >
+            {children === undefined ? text : children}
+        </button>
+    );
+}
