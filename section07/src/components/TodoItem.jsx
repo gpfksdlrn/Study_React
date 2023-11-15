7bd5631706df0f9cd9b6d7307615a35ad05a753f
hyeran <gpfksdlrn@naver.com>
2023-11-15 18:08:37 +0900

    section07 TodoList 프로젝트 만들기(1)

---
+++ section07/src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import './TodoItem.css';
+
+export default function TodoItem({
+    id, 
+    isDone, 
+    createdDate, 
+    content,
+    onUpdate,
+}){
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    }
+
+    return(
+        <div className='TodoItem'>
+            <input type="checkbox" checked={isDone} onChange={onChangeCheckbox}/>
+            <div className='content'>{content}</div>
+            <div className='date'>{new Date(createdDate).toLocaleDateString()}</div>
+            <button>삭제</button>
+        </div>
+    );
+}
