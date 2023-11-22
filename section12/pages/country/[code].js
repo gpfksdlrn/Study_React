6c04530b1b39379b3cde078a0285f361e142afe9
hyeran <gpfksdlrn@naver.com>
2023-11-22 15:36:42 +0900

    section14 SSR(서버 사이드 렌더링) 적용

--- section12/pages/country/[code].js
+++ section12/pages/country/[code].js
@@ -1,10 +1,37 @@
+import { fetchCountry } from "@/api";
 import SubLayout from "@/components/SubLayout";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Country(){
+export default function Country({country}){
     const router = useRouter();
     const {code} = router.query;
-    return <div>Country {code}</div>;
+    console.log("2");
+    useEffect(()=>{
+        console.log("3");
+    },[]);
+    
+    return (
+        <div>
+            {country.commonName} {country.officialName}
+        </div>
+    );
 }
 
 Country.Layout = SubLayout;
+
+export const getServerSideProps = async (context) => {
+    const {code} = context.params;
+    console.log("1");
+    let country = null;
+
+    if(code){
+        country = await fetchCountry(code);
+    }
+
+    return {
+        props: {
+            country
+        }
+    }
+}
--- section12/pages/index.js
+++ section12/pages/index.js
@@ -1,36 +1,32 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { fetchCountries } from "@/api";
+import { useEffect } from "react";
 
-export default function Home() {
-  const code = "KOR";
-  const router = useRouter();
-  const onClickButton=()=>{
-    //router.push("/search");
+export default function Home({countries}) {
+  console.log("Home");
+  
+  useEffect(()=>{
+    console.log("Home Mount");
+  }, []);
 
-    // replace는 뒤로가기를 방지하면서 페이지 이동
-    router.replace({
-      pathname: "/country/[code]",
-      query: {code: code}
-    })
-  }
   return (
     <div>
-      Home Page
-      <div>
-        <button onClick={onClickButton}>Search 페이지로 이동</button>
-      </div>
-      <div>
-        <Link href={"/search"}>Search Page 이동</Link>
-      </div>
-      <div>
-        <Link href={`/country/${code}`}>{code} 페이지로 이동</Link>
-        <br/>
-        {/* URL 오브젝트 */}
-        <Link href={{
-          pathname:"/country/[code]",
-          query:{code: code}
-        }}>{code} 페이지로 이동</Link>
-      </div>
+      {countries.map((country)=>(
+        <div key={country.code}>{country.commonName}</div>
+      ))}
     </div>
   );
-}
+};
+
+// 이 파일이 담당하는 페이지는 SSR방식으로 동작
+export const getServerSideProps = async() => {
+  // SSR을 위해 서버측에서 페이지 컴포넌트(Home)에게 전달할 데이터를 설정하는 함수
+
+  const countries = await fetchCountries();
+
+  console.log("getServerSideProps Called");
+  return{
+    props: {
+      countries
+    },
+  };
+};
